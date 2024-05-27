c16f31eed95759a0dbd49ca52aafb3c2a1ee7692
hyeonw8 <whgusrud223@gmail.com>
2024-05-27 17:27:36 +0900

    ✨ Context API로 상태 관리를 하기 위한 ContextProvider 컴포넌트 생성

---
+++ src/context/TextContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useEffect, useState } from "react";
+
+export const TextContext = createContext();
+
+export const TextProvider = ({ children }) => {
+  const [texts, setTexts] = useState(() =>
+    localStorage.getItem("texts")
+      ? JSON.parse(localStorage.getItem("texts"))
+      : [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem("texts", JSON.stringify(texts));
+  }, [texts]);
+
+  const onAddText = (text) => {
+    setTexts((prevTexts) => [...prevTexts, text]);
+  };
+
+  return (
+    <TextContext.Provider value={{ texts, setTexts, onAddText }}>
+      {children}
+    </TextContext.Provider>
+  )
+}
